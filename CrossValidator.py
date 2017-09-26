c7ecc5e86dbdc9a2ce1a993b6bbe75ca3ad1e9e8
demoonism <xianshanmingshi@msn.com>
2017-09-25 23:26:12 -0400

    Merging dev branch, clean up dir.

--- CrossValidator.py
+++ CrossValidator.py
@@ -31,35 +31,49 @@ class CrossValidatorVerbose(CrossValidator):
         print("total of", numModels, " models")
               
         eva = self.getOrDefault(self.evaluator)
-        validation = self.getOrDefault(self.valid)
 
         #metricName = eva.getMetricName()
         metricName = "AP"
         #print("metric is ", metricName)
-              
+        
         nFolds = self.getOrDefault(self.numFolds)
         seed = self.getOrDefault(self.seed)
+        h = 1.0 / nFolds
         metrics = [0.0] * numModels
+        
+        train = dataset.where(col("type") == "train").persist()
+        valid = dataset.where(col("type") == "valid").persist()
+        
+        randCol = self.uid + "_rand"
+        df = train.select("*", rand(seed).alias(randCol))
 
-        print("train size ", dataset.count())
-        print("valid size ",validation.count())
+        for i in range(nFolds):
+            foldNum = i + 1
+            print("Comparing models on fold %d" % foldNum)
 
-        for j in range(numModels):
-            paramMap = epm[j]
-            model = est.fit(dataset, paramMap)
+            LeaveOffLB = i * h
+            LeaveOffUB = (i + 1) * h
+            condition = (df[randCol] >= LeaveOffLB) & (df[randCol] < LeaveOffUB)
+            train = df.filter(~condition)
+            print("train size ", train.count())
+            print("valid size ",valid.count())
+                        
+            for j in range(numModels):
+                paramMap = epm[j]
+                model = est.fit(train, paramMap)
 
-            predictions = model.transform(validation, paramMap)
-            #print(predictions.show())
-            metric = eva.evaluate(predictions)
-            metrics[j] += metric
+                predictions = model.transform(valid, paramMap)
+                #print(predictions.show())
+                metric = eva.evaluate(predictions)
+                metrics[j] += metric
 
-            avgSoFar = metrics[j]
+                avgSoFar = metrics[j] / foldNum
 
-            res=("params: %s\t%s: %f\tavg: %f" % (
-                {param.name: val for (param, val) in paramMap.items()},
-                metricName, metric, avgSoFar))
-            result.append(res)
-            print(res)
+                res=("params: %s\t%s: %f\tavg: %f" % (
+                    {param.name: val for (param, val) in paramMap.items()},
+                    metricName, metric, avgSoFar))
+                result.append(res)
+                print(res)
 
         if eva.isLargerBetter():
             bestIndex = np.argmax(metrics)
